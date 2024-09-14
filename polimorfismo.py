ae5257a90eccd22d5e0757d77f96cc0ca374c27c
Coquinhaaa <juliano.ostroski@gmail.com>
2024-09-14 18:52:21 -0400

    Adição do arquivo de pratica da aula 026

---
+++ polimorfismo.py
@@ -0,0 +1,20 @@
+class Animal:
+    mamifero = True
+    def emitir_som(self):
+        print('Qualquer som...')
+
+class Cachorro(Animal):
+
+    def emitir_som(self):
+        print('Au Au!')
+
+class Gato(Animal):
+
+    def emitir_som(self):
+        print('Miau!')
+
+cachorro = Cachorro()
+cachorro.emitir_som()
+gato = Gato()
+gato.emitir_som()
+print(gato.mamifero)
